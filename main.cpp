581f277b857aee088333aadd18446fa9561e9f13
VugluskrDevNull <sofadiver@gmail.com>
2021-01-07 08:55:39 +0200

    simple web chat

--- main.cpp
+++ main.cpp
@@ -4,9 +4,95 @@
 #include <QThread>
 #include  <QAbstractSocket>
 using namespace std;
-//  послать боту пять строчек, каждую секунду, "hello <число>", где число от 5 до 0, и под конец послать "boom!" и отключится
 
- int main()
+int main()
+{
+    QTcpSocket *socket;
+
+    socket = new QTcpSocket(NULL);
+    socket->connectToHost("127.0.0.1", 4567);
+
+    if (!socket->waitForConnected(1000))
+    {
+        qDebug() << "Not Connected";
+        return 1;
+    }
+
+    qDebug() << "Connected";
+    while (1) {
+        socket->write("input> ");
+
+        while(socket->bytesAvailable() == 0) {
+            socket->waitForReadyRead(100);
+        }
+
+        qDebug() << "Reading" << socket->bytesAvailable();
+
+        QString c = socket->readLine();
+        qDebug() << c;
+
+        if (c.simplified().compare("exit") == 0)
+            break;
+
+        QString d = "you type: " + c;
+        socket->write(d.toLocal8Bit().constData());
+    }
+
+    socket->close();
+
+    return 0;
+}
+
+/*
+int main()
+ {
+    QTcpSocket * socket;
+    socket = new QTcpSocket(NULL);
+    socket->connectToHost("127.0.0.1", 4567);
+    if (socket->waitForConnected (1000))
+    {
+        qDebug() << "Connected";
+        while (1){
+        socket->write("input> ");
+         socket->waitForBytesWritten(1000);
+        while(socket->bytesAvailable() == 0) { socket->waitForReadyRead(100); }
+         qDebug()<<"Reading"<<socket->bytesAvailable();
+         QString c = socket->readLine(10);
+         qDebug()<<c;
+         QString d="you type: "+c;
+         char const* ch = d.toLocal8Bit().constData();
+         socket->write(ch);
+         if (strcmp(ch,"exit")==0) socket->close();
+        }
+
+    }
+    else  {  qDebug()<<"Not Connected";
+  }
+}
+*/
+/*
+int main()
+ {
+    QTcpSocket * socket;
+    socket = new QTcpSocket(NULL);
+    socket->connectToHost("127.0.0.1", 4567);
+    if (socket->waitForConnected (1000))
+    {
+        qDebug() << "Connected";
+         socket->write("hello\n");
+         socket->waitForBytesWritten(1000);
+        while(socket->bytesAvailable() == 0) { socket->waitForReadyRead(100); }
+         qDebug()<<"Reading"<<socket->bytesAvailable();
+         //qDebug()<<socket->readLine(10);
+         qDebug()<<socket->readAll();
+         socket->close();
+    }
+    else  {  qDebug()<<"Not Connected";
+  }
+}
+*/
+/*
+int main()
  {
     QTcpSocket * socket;
     socket = new QTcpSocket(NULL);
@@ -30,41 +116,6 @@ using namespace std;
     }
     else  {  qDebug()<<"Not Connected";    }
   }
+*/
 
 
- /*
-int main()
-{
-   QTcpSocket * socket;
-   socket = new QTcpSocket(NULL);
-   socket->connectToHost("62.149.7.206", 6660);    // 62.149.7.200/29
-   if (socket->waitForConnected (1000))
-   {
-       qDebug() << "Connected";
-       socket->write(" /nick testB /motd /join #politics_open /privmsg #politics_open hello! /quit");
-       socket->waitForBytesWritten();
-       socket->close();
-   }
-   else  {  qDebug()<<"Not Connected";    }
-  // void QThread::sleep (unsigned long  secs);
-    QThread::sleep (1);
-}
-*/
-/*
-int main()
-{
-   QTcpSocket * socket;
-  // socket = new QTcpSocket(this);
-     socket = new QTcpSocket(NULL);
-   socket->connectToHost("127.0.0.1", 4567);
-   if (socket->waitForConnected (1000))
-   {
-       qDebug() << "Connected";
-       //flush();
-       socket->write("hello\r\n");
-       socket->waitForBytesWritten();
-       socket->close();
-   }
-   else  {  qDebug()<<"Not Connected";    }
-}
-*/
