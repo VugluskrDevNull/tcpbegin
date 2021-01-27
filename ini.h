b26d0b977aba2da3e719d52104dc03d73034a551
VugluskrDevNull <sofadiver@gmail.com>
2021-01-27 18:55:30 +0200

    добавлен препроцессор данные сервера - константы

--- ini.h
+++ ini.h
@@ -1,5 +1,10 @@
+#ifndef INI_H
+#define INI_H   
+
 QFile init_file("bot_data.txt");
-    int port = 6667;
-    QString server = "irc.lucky.net";      // "62.149.7.206";  "irc.lucky.net"  "chat.freenode.net"
-    QString nick ="test_bot";
-    QString ircbot_channel = "#ruschat";
+const int port = 6667;
+const QString server = "irc.lucky.net";      // "62.149.7.206";  "irc.lucky.net"  "chat.freenode.net"
+const  QString nick ="test_bot";
+const QString ircbot_channel = "#ruschat";
+
+#endif
